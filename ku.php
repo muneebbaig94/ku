0cc2168658f159f834a350e5b176d131af4b58a4
muneebbaig94 <muhammadbaig94@gmail.com>
2016-03-30 21:02:59 +0500

    Added files via upload

---
+++ ku.php
@@ -0,0 +1,268 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  
+   <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+ 
+ <link rel="stylesheet" href="style.css">
+    </head>
+ 
+ <?php
+  <div class="container-fluid">
+    <nav class="navbar navbar-inverse ">
+      
+      <div  id="DIV2">
+       
+
+  
+       <img src="logo.png" alt="" id="IMG_5" />
+        <div class="row">
+           <div class="col-sm-4"></div>
+  
+    
+         <div class="col-sm-8">
+        <ul id="UL_8">
+          <li id="LI_9">
+            <span id="SPAN_11">Email</span>
+          </li>
+          <li id="LI_12">
+            <span id="SPAN_14">Facebook</span>
+          </li>
+          <li id="LI_15">
+            <span id="SPAN_14">MCN</span>
+          </li>
+          <li id="LI_18">
+          <span id="SPAN_14">Sitemap</span>
+          </li>
+          <li id="LI_21">
+          <span id="SPAN_14">Directory</span>
+          </li>
+        </ul>
+      
+      </div>
+    
+   </div>
+   <div class="row">
+    <div class="col-sm-8"></div>
+    <div class="col-sm-4"> <h3>call : (9221) 9926 1300-7</h3>
+  
+     </div>
+
+ </div>
+ <div class="row">
+  <div class="col-md-8"></div>
+  <div class="col-sm-4"> <input type="text"  placeholder="Search" />
+                               
+                                    <button  type="button">
+                                        <span class="glyphicon glyphicon-search"></span>
+                                    </button>
+                                </div>
+    </div>
+   <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span> 
+      </button>
+       <a class="navbar-brand" href="#">about us</a>
+      </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+      
+        <li class="dropdown">
+          <a class="dropdown-toggle" data-toggle="dropdown" href="#">HOME <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">ABOUT US</a></li>
+            <li><a href="#">campus news</a></i>
+            <li><a href="#">convocation</a></li>
+          </ul>
+        </li>
+         <li class="dropdown">
+          <a class="dropdown-toggle" data-toggle="dropdown" href="#">ADMISSION<span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">MORNING</a></li>
+            <li><a href="#">PHARM-D</a></li>
+            <li><a href="#">EVENING</a></li>
+          </ul>
+        </li>
+         <li class="dropdown">
+          <a class="dropdown-toggle" data-toggle="dropdown" href="#">FACULTY <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">ISLAMIC STUDIES</a></li>
+            <li><a href="#">LAW</a></li>
+            <li><a href="#">ENGINEERING</a></li>
+          </ul>
+        </li>
+         <li class="dropdown">
+          <a class="dropdown-toggle" data-toggle="dropdown" href="#">LIBRARY<span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">HOME</a></li>
+            <li><a href="#">DIGITAL</a></li>
+            <li><a href="#">EBRARY</a></li>
+          </ul>
+        </li>
+        
+          <li class="dropdown">
+          <a class="dropdown-toggle" data-toggle="dropdown" href="#">ADMINISTRATION<span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">VICE CHANCELLOR</a></li>
+            <li><a href="#">REGISTRAR</a></li>
+            <li><a href="#">FORMER VICE CHANCELLOR</a></li>
+          </ul>
+        </li>
+          <li class="dropdown">
+          <a class="dropdown-toggle" data-toggle="dropdown" href="#">EXAMINATION<span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">ANNUAL SYSTEM</a></li>
+            <li><a href="#">SEMESTER SYSTEM</a></li>
+            
+          </ul>
+        </li>
+         
+          <a class="navbar-brand" href="#">ALUMNI </a>
+         
+      
+      </ul>
+ 
+   
+    </div>
+    </nav>
+   
+
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+      <li data-target="#myCarousel" data-slide-to="3"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner" role="listbox">
+      <div class="item active">
+        <img src="newbanner444.jpg" alt="Chania" width="460" height="345">
+      </div>
+
+      <div class="item">
+        <img src="ppl3.jpg" alt="Chania" width="460" height="345">
+      </div>
+    
+      <div class="item">
+        <img src="banner4.jpg" alt="Flower" width="460" height="345">
+      </div>
+
+      <div class="item">
+        <img src="banner-usaid.jpg" alt="Flower" width="460" height="345">
+      </div>
+      </div>
+
+    
+   
+    
+    </div>
+     <div class="row">
+
+    <div class="col-sm-4">
+      <span id="A_188">ALUMNI REGISTRATION</span>
+    </div>
+    <div class="col-sm-8"></div>
+  </div>
+  <div class="row">
+
+    <div  class="col-sm-8" style="height:41px;">
+      
+      <img src="bg-heading-news.png" alt="" id="IMG6" />
+
+          Active Citizen Certificate Course : 
+          <SPAN id="A_205">Advertisment</SPAN>
+       </div>
+       <div class="col-sm-4"></div>
+  </div>
+  <div class="row">
+
+  <div  class="col-sm-12" >
+  
+     <p >  <img src="bg-1.png" alt="" id="IMG6" /> <span style="color:green">welcome</span>
+      <span style:"font-size:12px">At the time of creation of Pakistan as a sovereign state in 1947, means for higher education and research were negligible. </span></p>
+  </div>
+  
+ </div >
+ <div class="jumbotron" >
+  
+   
+   <div style="  height: 238px;
+    width: 350px;      float: left;">
+     <div style="    height: 13px;
+    width: 175px; background-color: white;     float: left;">
+    <h4 style=" padding:0px; color: green;">LATEST RESULT</h4>
+    </div>
+    <div style="    height: 29px;
+    width: 175px; background-color: green;     float: left;">
+  
+</div>
+         
+ <table class="table table-striped"
+    
+    <tbody>
+      <tr>
+        <td><pre><span style="color: green" >PM LAPTOP SCEME</span>  
+        <span style="color: green" >page 1   </span>   </pre>  </td>
+          </tr>
+          <tr>
+        <td><pre><span style="color: green" >BS  morning based admission list</span>   
+           </pre>  </td>
+          </tr>
+          <tr>
+           </tbody>
+          </table>
+  
+        </div>
+        
+        <div  style="padding-left :150px;"   >
+        <div style="    float: LEFT;
+    height: 260px;
+    width: 350px; ">
+    <div id="DIV_356"> <h3 style="  color: green;">department</h3>
+    </div>
+ <form style="width: 100%;
+    padding: 60px 70px;
+    border: none;
+   
+   ">
+  <select id="country" name="country">
+    <option value="usa">computer science</option>
+    <option value="usa">business</option>
+    <option value="usa">mathematics</option>
+  </select>
+ <span class="glyphicon glyphicon-search">
+  <input type="submit"  value="g0">
+</form>
+
+</div>
+              
+                 
+           <div style="    float: right;
+    height: 248px;
+    width: 157px;">
+    <img src="banner-joinus-QK7b22.jpg" alt="" id="IMG8"  />
+   </div>
+   
+   </div>
+</div>
+
+  <img class="img-responsive"src="footerend.png" alt="Cinque Terre" width="1000" height="236">
+  
+
+
+</div>
+   
+
+?>
+</body>
+</html>
